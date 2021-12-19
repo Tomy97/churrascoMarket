ca17c13277dc08a0e6905bab5a7ed0a43dff2a5a
Tomy97 <tomas.pandu@gmail.com>
2021-12-19 06:11:23 -0300

    correcciones

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <HelloWorld/>
+    <Login />
   </div>
 </template>
 
 <script>
 import HelloWorld from '../components/HelloWorld.vue';
+import Login from '../views/Login.vue';
 
 export default {
   name: 'Home',
   
   components: {
     HelloWorld,
+    Login
   },
 };
 </script>

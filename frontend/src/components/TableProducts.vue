9b3370aa1f712d8f5de3d49f1ee43ffa8b396761
Tomas <tomas.pandu@gmail.com>
2022-07-15 17:43:41 -0300

    adsadsda

--- frontend/src/components/TableProducts.vue
+++ frontend/src/components/TableProducts.vue
@@ -1,72 +1,85 @@
 <template>
   <v-container>
-    <v-simple-table class="tableWidth">
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th class="text-center">Name</th>
-            <th class="text-center">Precios</th>
-            <th class="text-center">Divisa</th>
-            <th class="text-center"></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="i in datos" :key="i.id">
-            <td class="text-center">{{ i.name }}</td>
-            <td class="text-center">{{ i.precios }}</td>
-            <td class="text-center">{{ i.divisa }}</td>
-            <td class="text-center">
-              <ModalComponent
-                title="Ver productos"
-                :name="i.name"
-                :divisa="i.divisa"
-                :price="i.precios"
-              />
-            </td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+    <v-row>
+      <v-col cols="12" md="12">
+        <v-simple-table class="tableWidth">
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-center">Name</th>
+                <th class="text-center">Precios</th>
+                <th class="text-center">Divisa</th>
+                <th class="text-center"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="i in datos" :key="i.id">
+                <td class="text-center">{{ i.name }}</td>
+                <td class="text-center">{{ i.precios }}</td>
+                <td class="text-center">{{ i.divisa }}</td>
+                <td class="text-center">
+                  <ModalComponent
+                    title="Ver productos"
+                    :name="i.name"
+                    :divisa="i.divisa"
+                    :price="i.precios"
+                  />
+                </td>
+                <td>
+                  <v-btn color="error" @click="handleDeleteProduct(i.id)">
+                    Eliminar Producto
+                  </v-btn>
+                </td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 <script>
-import ModalComponent from './ModalComponent.vue'
+import ModalComponent from "./ModalComponent.vue";
 export default {
   data: () => ({
     datos: [
       {
         id: 1,
-        name: 'Jamon',
+        name: "Jamon",
         precios: 123,
-        divisa: 'Dolar',
+        divisa: "Dolar",
       },
       {
         id: 2,
-        name: 'Queso',
+        name: "Queso",
         precios: 12,
-        divisa: 'Pesos',
+        divisa: "Pesos",
       },
       {
         id: 3,
-        name: 'Aceituna',
+        name: "Aceituna",
         precios: 54,
-        divisa: 'Dolar',
+        divisa: "Dolar",
       },
       {
         id: 4,
-        name: 'Valde',
+        name: "Valde",
         precios: 64,
-        divisa: 'Pesos',
+        divisa: "Pesos",
       },
     ],
   }),
   methods: {
     handleClick(i) {
-      console.log(i)
+      console.log(i);
+    },
+    async handleDeleteProduct(id) {
+      this.datos = await this.datos.filter((item) => item.id !== id);
+      console.log('se eliminooo!!! ');
     },
   },
   components: { ModalComponent },
-}
+};
 </script>
 <style>
 .tableWidth {
